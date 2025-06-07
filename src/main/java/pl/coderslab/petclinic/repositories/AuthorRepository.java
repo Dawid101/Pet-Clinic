c98dc8a4e180a4f9f07c8b02ceae5bb846e9a095
dawid101 <dawcik35@gmail.com>
2025-06-07 17:39:19 +0200

    czyszczenie projektu

--- src/main/java/pl/coderslab/petclinic/repositories/AuthorRepository.java
+++
@@ -1,8 +0,0 @@
-package pl.coderslab.petclinic.repositories;
-
-import org.springframework.data.repository.CrudRepository;
-import pl.coderslab.petclinic.domain.Author;
-
-
-public interface AuthorRepository extends CrudRepository<Author, Long> {
-}
--- src/main/java/pl/coderslab/petclinic/repositories/PublisherRepository.java
+++
@@ -1,7 +0,0 @@
-package pl.coderslab.petclinic.repositories;
-
-import org.springframework.data.repository.CrudRepository;
-import pl.coderslab.petclinic.domain.Publisher;
-
-public interface PublisherRepository extends CrudRepository<Publisher, Long> {
-}
